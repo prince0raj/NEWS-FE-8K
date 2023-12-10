ada6b1b765551764f03ef5ff3b6dd1738c14ee03
prince raj <@gmail.com>
2023-12-10 14:29:12 +0530

    Api ofr Hero Page is done

--- src/components/home/hero/Card.jsx
+++ src/components/home/hero/Card.jsx
@@ -1,27 +1,47 @@
-import React from "react"
-import { Link } from "react-router-dom"
-
-const Card = ({ item: { id, cover, catgeory, title, authorName, time } }) => {
+import React from "react";
+import { Link } from "react-router-dom";
+import { BASE_URL } from "../../../Domain";
+const Card = ({
+  item: { id, cover, title, author_name, created_at },
+  catgeory,
+}) => {
+  const date = new Date(created_at);
+  var month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   return (
     <>
-      <div className='box'>
-        <div className='img'>
-          <img src={cover} alt='' />
+      <div className="box">
+        <div className="img">
+          <img src={`${BASE_URL}${cover}`} alt="" />
         </div>
-        <div className='text'>
-          <span className='category'>{catgeory}</span>
+        <div className="text">
+          <span className="category">{catgeory}</span>
           {/*<h1 className='titleBg'>{title}</h1>*/}
           <Link to={`/SinglePage/${id}`}>
-            <h1 className='titleBg'>{title}</h1>
+            <h1 className="titleBg">{title}</h1>
           </Link>
-          <div className='author flex'>
-            <span>by {authorName}</span>
-            <span>{time}</span>
+          <div className="author flex">
+            <span>by {author_name}</span>
+            <span>{`${date.getDate()}-${
+              month[date.getMonth()]
+            }-${date.getFullYear()}`}</span>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,26 +1,59 @@
-import React, { useState } from "react"
-import { hero } from "../../../dummyData"
-import "./hero.css"
-import Card from "./Card"
-
+import React, { useState, useEffect } from "react";
+import { hero } from "../../../dummyData";
+import "./hero.css";
+import Card from "./Card";
+import {
+  getBusiness,
+  getEconomics,
+  getEntertainment,
+  getLifestyle,
+} from "../../../Routes/getrequest";
 const Hero = () => {
-  const [items, setIems] = useState(hero)
-
+  const [items, setIems] = useState(hero);
+  const [state, setstate] = useState([]);
+  let val = true;
+  useEffect(() => {
+    // let data = [];
+    const fetchData = async () => {
+      try {
+        const [res1, res2, res3, res4] = await Promise.all([
+          new Promise((resolve, reject) =>
+            getBusiness((err, res) => (err ? reject(err) : resolve(res)))
+          ),
+          new Promise((resolve, reject) =>
+            getEconomics((err, res) => (err ? reject(err) : resolve(res)))
+          ),
+          new Promise((resolve, reject) =>
+            getEntertainment((err, res) => (err ? reject(err) : resolve(res)))
+          ),
+          new Promise((resolve, reject) =>
+            getLifestyle((err, res) => (err ? reject(err) : resolve(res)))
+          ),
+        ]);
+        // console.log(res1);
+        setstate([res1.data[0], res2.data[0], res3.data[0], res4.data[0]]);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  const arr = ["BUSINESS", "ECONOMIC", "ENTERTAINMENT", "LIFESTYLE"];
   return (
     <>
-      <section className='hero'>
-        <div className='container'>
-          {items.map((item) => {
+      <section className="hero">
+        <div className="container">
+          {state.map((item, ind) => {
             return (
               <>
-                <Card key={item.id} item={item} />
+                <Card key={ind} item={item} catgeory={arr[ind]} />
               </>
-            )
+            );
           })}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
